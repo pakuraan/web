cb1d6ba3028a897724c6a2c71e7cb700f07e3196
pakuraan <javednabeegh@gmail.com>
2025-01-16 16:55:10 +0500

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -5,24 +5,21 @@ const data = {
     siteurl: "pakuraan.org.pk",
     sitelogo: "",
     title: "Coming Soon!",
-    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches🚀 that we think you&apos;ll love 😍.",
+    description: "We're currently working 👨‍💻 hard to bring you something great, and we can't wait to share it 📅. Our team is putting the finishing touches🚀 that we think you'll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
     copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com">Nabeegh</a>`,
     socialIconsHeading: "Follow Us 📣",
-    hideSubscribeForm: false , // make true to disable subscription form 
+    hideSubscribeForm: false, // make true to disable subscription form
     socialIcons: [
         {
             icon: "facebook",
             link: "https://www.facebook.com/people/PakUraan/61555639025751/",
         },
         {
-        },
-        {
             icon: "linkedIn",
             link: "https://www.linkedin.com/company/pakuraan/?originalSubdomain=pk",
         },
     ],
-   
-}
+};
 
 export default data;
