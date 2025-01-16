8766bbb53c42c59ab441e73df84d7645b6d51150
pakuraan <javednabeegh@gmail.com>
2025-01-16 16:24:50 +0500

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -5,23 +5,21 @@ const data = {
     siteurl: "pakuraan.org.pk",
     sitelogo: "",
     title: "Coming Soon!",
-    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches o 🚀 that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
-    copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>`,
+    copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com">Nabeegh</a>`,
     socialIconsHeading: "Follow Us 📣",
     hideSubscribeForm: false, // make true to disable subscription form 
     socialIcons: [
         {
             icon: "facebook",
-            link: "https://www.facebook.com/sandeep.baikare/",
+            link: "https://www.facebook.com/people/PakUraan/61555639025751/",
         },
         {
-            icon: "twitter",
-            link: "https://twitter.com/BaikareSandeep",
         },
         {
             icon: "linkedIn",
-            link: "https://www.linkedin.com/in/sandip-baikare/",
+            link: "https://www.linkedin.com/company/pakuraan/?originalSubdomain=pk",
         },
     ],
     hide :{
