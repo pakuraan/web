b5d62fe3bafc5aaf164d92e561a8e8af8e96381a
pakuraan <javednabeegh@gmail.com>
2025-01-16 16:33:09 +0500

    Update Navigation.tsx

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -6,8 +6,7 @@ function Navigation() {
     <>
     <nav className="flex sm:justify-center space-x-4">
         {[
-            ['Home', '/'],
-            ['Version 1', '/version1'],
+            ['Home', '/']
         ].map(([title, url]) => (
             <a key={title} href={url} className="rounded-lg px-3 py-2 font-semibold text-slate-800 hover:bg-slate-700 hover:text-slate-100 dark:text-slate-100 dark:hover:text-slate-800 dark:hover:bg-zinc-300">{title}</a>
         ))}
